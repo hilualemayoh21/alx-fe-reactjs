8180bb84645d649228fe1978a0e56a594bb6722b
hlina alemayoh <hilubecoder@gmail.com>
2024-11-01 16:30:00 +0300

    add additional correction

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,7 +6,8 @@ import WelcomeMessage from "./components/WelcomeMessage";
 import Header from "./components/Header.jsx";
 import MainContent from "./components/MainContent.jsx";
 import Footer from "./components/Footer.jsx";
-import UserProfile from "./components/UserProfile .jsx";
+import UserProfile from "./components/UserProfile.jsx";
+
 function App() {
   const [count, setCount] = useState(0);
 
