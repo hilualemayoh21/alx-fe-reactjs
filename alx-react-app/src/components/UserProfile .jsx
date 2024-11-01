933b2ac1e0e6b006d5e04139f4460543d4ed84c3
hlina alemayoh <hilubecoder@gmail.com>
2024-11-01 16:13:05 +0300

    add the some correction to the userprofile component

--- alx-react-app/src/components/UserProfile .jsx
+++ alx-react-app/src/components/UserProfile .jsx
@@ -1,9 +1,9 @@
 const UserProfile = (props) => {
   return (
     <div>
-      <h2>props.name</h2>
-      <p>props.age</p>
-      <p>props.bio</p>
+      <h2>{props.name}</h2>
+      <p>Age: {props.age}</p>
+      <p>Bio: {props.bio}</p>
     </div>
   );
 };
