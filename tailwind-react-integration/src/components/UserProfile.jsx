2673af625bf992384f1b0e338915ed81de5abf06
hlina alemayoh <hilubecoder@gmail.com>
2024-11-28 10:25:59 +0300

    add rounding to the container

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-8 max-w-sm mx-auto my-20 shadow-lg">
+    <div className="bg-gray-100 p-8 max-w-sm mx-auto my-20 shadow-lg rounded-lg">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
