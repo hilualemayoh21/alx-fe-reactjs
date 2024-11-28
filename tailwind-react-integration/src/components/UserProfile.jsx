b008fcbe6c1e01cc706b17dfbe72ff5a8ff4d221
hlina alemayoh <hilubecoder@gmail.com>
2024-11-28 11:48:46 +0300

    add hover effect

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,12 +1,14 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100 sm:p-4 md:p-8 md:max-w-sm  lg:max-w-lg mx-auto my-20 shadow-lg rounded-lg sm:max-w-xs ">
+    <div className="bg-gray-100 sm:p-4 md:p-8 md:max-w-sm  lg:max-w-lg mx-auto my-20 shadow-lg rounded-lg sm:max-w-xs  hover:shadow-lg">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="rounded-full md:w-36 md:h-36 mx-auto sm:w-24 sm:h-24 object-contain"
+        className="rounded-full md:w-36 md:h-36 mx-auto sm:w-24 sm:h-24 object-contain hover:scale-110 transition-transform duration-300 ease-in-out"
       />
-      <h1 className="md:text-xl sm:text-lg text-blue-800 my-4">John Doe</h1>
+      <h1 className="md:text-xl sm:text-lg text-blue-800 my-4 hover:text-blue-500">
+        John Doe
+      </h1>
       <p className="text-gray-600 text-base sm:text-sm">
         Developer at Example Co. Loves to write code and explore new
         technologies.
