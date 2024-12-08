259f9c782623480b97514b1bc95ba306285c2d81
hlina alemayoh <hilubecoder@gmail.com>
2024-12-08 23:28:53 +0300

    add style

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -31,7 +31,8 @@ const HomePage = () => {
 
   return (
     <div className="container mx-auto p-5">
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
+      {/* Use Tailwind's responsive grid classes */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
         {data.map((recipe) => (
           <div
             key={recipe.id}
